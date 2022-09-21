feb79923a093aae463214d4c4498adc6a267cadd
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-09-21 23:02:09 +0300

    add1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const divGalleryRef = document.querySelector('.gallery');
+const galleryImage = createGalleryImage(galleryItems);
+divGalleryRef.insertAdjacentHTML('beforeend', galleryImage);
+function createGalleryImage(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+    }).join(''); 
+};
+
+let gallery = new SimpleLightbox('.gallery a', { captions: true, captionDelay: 250, captionsData: 'alt' });
+gallery.on('show.simplelightbox', function () {
+	 
+});
