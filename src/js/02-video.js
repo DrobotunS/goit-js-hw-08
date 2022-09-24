d97108602bd4d9f3cfb16d8661f19c4012a44b85
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-09-24 16:32:16 +0300

    add2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,38 @@
+import throttle from "lodash.throttle";
 
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+let localStorageTime = "videoplayer-current-time";
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
+
+// const onPlay = function(data) {
+
+//     localstorage.setItem(localStorageTime, data.seconds)
+// };
+// player.on('timeupdate', onPlay);
+
+// const sevDataSec = localStorage.getItem(localStorageTime);
+// console.log(sevDataSec)
+player.on('timeupdate', function(data) {
+        console.log(data.seconds)
+        localstorage.setItem("videoplayer-current-time", data.seconds)
+    });
+    
+
+
+
+player.setCurrentTime(localStorageTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            on
+            break;
+
+        default:
+      
+            break;
+    }
 });
