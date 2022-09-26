7467be457917f47670b091e50db4fdd5d705efde
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-09-26 21:18:05 +0000

    Deploying to gh-pages from @ DrobotunS/goit-js-hw-08@aa1677307cff58623386004a217bf761361bdb60 🚀

--- 03-feedback.214bca95.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t=document.querySelector(".feedback-form textarea");e.addEventListener("submit",(function(e){})),t.addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-mesaage",t),console.log(t)}))}();
-//# sourceMappingURL=03-feedback.214bca95.js.map
--- 03-feedback.e703e2bf.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),t=document.querySelector(".feedback-form textarea");e.addEventListener("submit",(function(e){})),t.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-mesaage",t),console.log(t)}));
-//# sourceMappingURL=03-feedback.e703e2bf.js.map
